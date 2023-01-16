09951d6524cd8f79d0eedbcf346c5eb27cf987df
Connor <connorzalud@gmail.com>
2023-01-16 15:05:19 -0500

    added ids to all book info, function to update rows based on new array data

--- javascript.js
+++ javascript.js
@@ -31,19 +31,22 @@ function addBookToList(){
 //add book info to DOM, check to see if div id already exists to avoid duplicating book info    
 function addBookItem(){
     for(i=0;i<myLibrary.length;i++){
-        if(document.getElementById("div" + i)){
+        if(document.getElementById("title" + i)){
             continue;
         }
         const titleDiv = document.createElement("div");
         const authorDiv = document.createElement("div");
         const pagesDiv = document.createElement("div");
         const readDiv = document.createElement("div");
-        titleDiv.setAttribute("id", "div" + i);
-        titleDiv.setAttribute("class", "book-row")
+        titleDiv.setAttribute("id", "title" + i);
+        titleDiv.setAttribute("class", "book-row");
+        authorDiv.setAttribute("id","author" + i);
         authorDiv.setAttribute("class","book-row");
+        pagesDiv.setAttribute("id", "pages"+i);
         pagesDiv.setAttribute("class","book-row");
+        readDiv.setAttribute("id","read"+i);
         readDiv.setAttribute("class","book-row");
-        titleDiv.innerText = myLibrary[i].title;
+        titleDiv.textContent = myLibrary[i].title;
         library.append(titleDiv);
         authorDiv.innerText = myLibrary[i].author;
         library.append(authorDiv);
@@ -53,6 +56,19 @@ function addBookItem(){
         library.append(readDiv);
 
 
+    }
+}
+
+function updateRow(){
+    for(i=0;i<myLibrary.length;i++){
+    let newTitle = document.querySelector("#title"+i);
+    newTitle.textContent = myLibrary[i].title;
+    let newAuthor = document.querySelector("#author"+i);
+    newAuthor.textContent=myLibrary[i].author;
+    let newPages = document.querySelector("#pages"+i);
+    newPages.textContent=myLibrary[i].pages;
+    let newRead = document.querySelector("#read"+i);
+    newRead = myLibrary[i].read;
     }
 }
 
