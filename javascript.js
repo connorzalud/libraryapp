ad49a71a26d6e87faac49020cc1e95fcf1bb3d0f
Connor <connorzalud@gmail.com>
2023-01-16 09:50:41 -0500

    book class, empty array, function to add book to array, function to add book info to container

--- javascript.js
+++ javascript.js
@@ -1,1 +1,37 @@
+const library = document.querySelector(".library-container");
+
+let myLibrary = []
+
+class Book {
+    constructor(title, author, pages, read){
+        this.title=title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
+}
+
+function addBookToList(title,author,pages,read){
+    const book = new Book(title,author,pages,read);
+    myLibrary.push(book)
+    }
+
+
+//add book info to DOM, check to see if div id already exists to avoid duplicating book info    
+function addBookItem(){
+    for(i=0;i<=myLibrary.length;i++){
+        if(document.getElementById("div" + i)){
+            continue;
+        }
+        const titleDiv = document.createElement("div");
+        const authorDiv = document.createElement("div");
+        titleDiv.setAttribute("id", "div" + i);
+        titleDiv.innerText = myLibrary[i].title;
+        library.append(titleDiv);
+        authorDiv.innerText = myLibrary[i].author;
+        library.append(authorDiv);
+
+    }
+}
 
